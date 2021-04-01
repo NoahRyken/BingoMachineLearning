8009aea7755c60a5a50d2a985aab46e811cdbd7b
NoahRyken <66643170+NoahRyken@users.noreply.github.com>
2021-04-01 09:36:02 -0500

    Added Buggy Win Detection & Easy Computer Opponent

--- connect4.py
+++ connect4.py
@@ -38,18 +38,70 @@ def printBoard():
 
 def lastMoveWin(column):
     global board
-    color = 0
+    global color
     inRow = 0
-    for row in board:
-        if row[column] != None and color == 0: 
-            color = row[column]
-        elif row[column] == color:
+    level = 0
+    winner = color*-1
+
+    for index in range(6):
+        if board[index][column] != None:
+            level = index
+            break
+    
+    for index in range(6):
+        if board[index][column] == winner:
             inRow += 1
-        elif row[column] == -1*color:
-            return False
+        if board[index][column] != winner:
+            inRow = 0
         if inRow == 4:
             return True
+
+    inRow = 0
+    for piece in board[level]:
+        if piece == winner:
+            inRow += 1
+        if piece != winner:
+            inRow = 0
+        if inRow == 4:
+            return True
+
+    inRow1 = 0
+    inRow2 = 0
+    locations = [level, column]
+    left1 = True
+    right1 = True
+    left2 = True
+    right2 = True
+
+    for index in range(7):
+        try:
+            if left1 and board[locations[0] + index][locations[1] - index] == winner:
+                inRow1 += 1
+            else:
+                left1 = False
+            if right1 and board[locations[0] - index][locations[1] + index] == winner:
+                inRow1 += 1
+            else:
+                right1 = False
+        except:
+            continue
+        try:
+            if left2 and board[locations[0] + index][locations[1] + index] == winner:
+                inRow2 += 1
+            else:
+                left2 = False
+            if right2 and board[locations[0] - index][locations[1] - index] == winner:
+                inRow2 += 1
+            else:
+                right2 = False
+        except:
+            continue
+
+    if inRow1 > 4 or inRow2 > 4:
+        return True
+
     return False
+    
 
 
 
--- server.py
+++ server.py
@@ -2,23 +2,119 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import connect4
+import random
+import time
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if request.form['gameSelect'] == 'Player Vs. Player':
+            return redirect('/playerVsPlayer')
+        elif request.form['gameSelect'] == 'Player Vs. Easy':
+            return redirect('/playerVsEasy')
+        elif request.form['gameSelect'] == 'Easy Vs. Easy':
+            return redirect('/easyVsEasy')
+    else:
+        return render_template("index.html")
+
+@app.route('/playerVsPlayer', methods=['GET', 'POST'])
+def playerVsPlayer():
     winner = 0
     if request.method == 'POST':
         try:
             if request.form['restart'] == 'Restart':
                 connect4.reset()
         except:
+            pass
+        try:
+            if request.form['home'] == 'Home':
+                connect4.reset()
+                connect4.color = -1
+                return redirect('/')
+        except:
+            pass
+        try:
             if int(request.form['column']) in range(8):
                 val = int(request.form['column']) - 1
                 connect4.moveAdder(val)
             if connect4.lastMoveWin(val):
                 winner = connect4.color
-    return render_template("index.html", display=connect4.board, winner=winner)
+        except:
+            pass
+    return render_template("playerVsPlayer.html", display=connect4.board, winner=winner, color=connect4.color)
+
+@app.route('/playerVsEasy', methods=['GET', 'POST'])
+def playerVsEasy():
+    winner = 0
+    if request.method == 'POST':
+        try:
+            if request.form['home'] == 'Home':
+                connect4.reset()
+                connect4.color = -1
+                return redirect('/')
+        except:
+            pass
+        try:
+            if int(request.form['column']) in range(8):
+                val = int(request.form['column']) - 1
+                connect4.moveAdder(val)
+            if connect4.lastMoveWin(val):
+                winner = connect4.color
+            else:
+                computer = random.randrange(0, 7)
+                connect4.moveAdder(computer)
+                if connect4.lastMoveWin(computer):
+                    winner = connect4.color
+        except:
+            pass
+        try:
+            if request.form['restart'] == 'Restart':
+                connect4.reset()
+                connect4.color = -1
+        except:
+            pass
+        return render_template("playerVsEasy.html", display=connect4.board, winner=winner, color=connect4.color)
+    else:
+        connect4.color = -1
+    return render_template("playerVsEasy.html", display=connect4.board, winner=winner, color=connect4.color)
+
+@app.route('/easyVsEasy', methods=['GET', 'POST'])
+def easyVsEasy():
+    winner = 0
+    if request.method == 'POST':
+        try:
+            if request.form['home'] == 'Home':
+                connect4.reset()
+                connect4.color = -1
+                return redirect('/')
+        except:
+            pass
+        try:
+            if request.form['start'] == 'Start':
+                while True:
+                    computer = random.randrange(0, 7)
+                    connect4.moveAdder(computer)
+                    if connect4.lastMoveWin(computer):
+                        winner = connect4.color
+                        break
+                    else:
+                        computer = random.randrange(0, 7)
+                        connect4.moveAdder(computer)
+                        if connect4.lastMoveWin(computer):
+                            winner = connect4.color
+                            break
+                    return render_template("easyVsEasy.html", display=connect4.board, winner=winner, color=connect4.color)
+        except:
+            pass
+        try:
+            if request.form['restart'] == 'Restart':
+                connect4.reset()
+                connect4.color = -1
+        except:
+            pass
+    return render_template("easyVsEasy.html", display=connect4.board, winner=winner, color=connect4.color)
 
 if __name__ == "__main__":
     app.run(debug=True)
